d55040243834a44c14029bbf4b16229e872784dc
horollee <horollee@gmail.com>
2023-07-23 17:23:36 +0900

    copilot test

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// practice project - portal to access apps
+// This is the root component of the app.
+// Design Reference : https://m3.material.io/get-started
+// Monitor Resolution Consideration
+// 1. 1920 x 1080
+//// standard resolution
+// 2. 1366 x 768
+//// change component size
+// 3. 3840 x 2160
+//// display all the components in a single page
+// 4. 1920 x 540
+//// component realignment
+// 5. 960 x 540
+//// minimum resolution
+//// hide some components
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="button">Button</button>
+      <button className="button">Button</button>
     </div>
   );
 }
